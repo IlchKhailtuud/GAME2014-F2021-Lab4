fca71a14417ec0e0ab21368af25e9cfd51d29bdf
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-09-17 22:55:25 -0400

    Added Start Btn to the main menu

---
+++ Assets/[Scripts]/StartBtnBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBtnBehaviour : MonoBehaviour
+{
+   public void OnStartBtnPress()
+   {
+      Debug.Log("Start Btn Pressed!");
+   }
+}
